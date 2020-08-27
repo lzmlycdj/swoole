61860878308080e3205965c568bbb5a23c167de8
lzm2013lzm <lzm2013lzm@gmail.com>
2020-08-27 14:16:13 +0800

    redis异步逻辑模糊匹配

--- redis.php
+++ redis.php
@@ -11,11 +11,19 @@ $client->connect('127.0.0.1', 6379, function (swoole_redis $client, $result) {
     );
     */
 
-    $client->get('singwa_1',function (swoole_redis $client, $result){
+    /* $client->get('singwa_1',function (swoole_redis $client, $result){
         var_dump($result);
         $client->close();
-    });
+    }); */
 
+// 获取所有的keys
+   /*  $client->keys('*',function (swoole_redis $client, $result){
+        var_dump($result);
+    }); */
+// redis异步模糊匹配
+     $client->keys('*gw*',function (swoole_redis $client, $result){
+        var_dump($result);
+    });
 });
 
 echo "start".PHP_EOL;
