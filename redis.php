48b56a3ed8f3f6245e45e3c811b5c7fff2b36d63
lzm2013lzm <lzm2013lzm@gmail.com>
2020-08-27 14:09:27 +0800

    异步redis读取键值

--- redis.php
+++ redis.php
@@ -2,13 +2,20 @@
 $client = new swoole_redis;
 $client->connect('127.0.0.1', 6379, function (swoole_redis $client, $result) {
   
-    echo "connect".PHP_EOL;
+    /* echo "connect".PHP_EOL;
     var_dump($result);
     $client->set('singwa_1',time(),
     function(swoole_redis $client,$result){
         var_dump($result);
-    }
-);
+    } 
+    );
+    */
+
+    $client->get('singwa_1',function (swoole_redis $client, $result){
+        var_dump($result);
+        $client->close();
+    });
+
 });
 
 echo "start".PHP_EOL;
